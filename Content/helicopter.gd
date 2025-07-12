4449fd9764ddd6a0c411e8394357bbe22af92380
Theodor <fracter.giftogod@gmail.com>
2025-07-12 22:49:54 +0200

    cleaned up helicopter code and added mass to thrust and rotation calculation. Also separated the color adjustments in World, and tweaked it a bit

--- Content/helicopter.gd
+++ Content/helicopter.gd
@@ -1,14 +1,22 @@
 extends RigidBody3D
 
-@export var speed_pitch:float = 10
-@export var speed_barrel:float = 10
-@export var speed_yaw:float = 10
-@export var speed_thrust:float = 10
-
 @export var flip_pitch : float = 1
+@export var multiply_by_mass: bool = true
+
+
+
+@export_group("Rotation", "rotation_")
+@export var rotation_multiplier: float = 1
+@export var rotation_pitch:float = 1
+@export var rotation_barrel:float = 1
+@export var rotation_yaw:float = 1
+
+@export_group("Thrust", "thrust_")
+@export var thrust_up:float = 4
 
 
 @onready var debug_thrust_pointer: MeshInstance3D = $DebugThrustPointer
+@onready var input_display: Label = $UI/InputDisplay
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -30,30 +38,13 @@ func _physics_process(delta: float) -> void:
 	var input_yaw:float = Input.get_axis(&"yaw_right", &"yaw_left")
 	var input_thrust:float = Input.get_axis(&"thrust_down", &"thrust_up")
 	
-	var rot:Vector3 = Vector3(input_pitch * speed_pitch, input_yaw * speed_yaw, input_barrel * speed_barrel)
-	
-	# Rotate the "rotation force" by the actual rotation (hopefully making it into local-rotation)
-	#var rot_matrix: Basis = 
-	
-	#var qua: Quaternion = global_basis.get_rotation_quaternion().normalized()
-	#var rot_matrix: Transform3D
-	#var rotaded_rot: Vector3 = rot * basis.orthonormalized()
-	#transform.basis.get_rotation_quaternion()
-	#transform.rotated_local().basis.get_rotation_quaternion()
-	# rotate "rot" by local-space rotation
-	
-	
-	var new_tr: Transform3D = Transform3D(transform)
-	new_tr = new_tr.rotated_local(Vector3.RIGHT, rot.x * delta)
-	new_tr = new_tr.rotated_local(Vector3.UP, rot.y * delta)
-	new_tr = new_tr.rotated_local(Vector3.FORWARD, rot.z * delta)
-	
-	#transform.basis = new_tr.basis.orthonormalized()
+	var rot:Vector3 = Vector3(input_pitch * rotation_pitch, input_yaw * rotation_yaw, input_barrel * rotation_barrel)
+	if multiply_by_mass: rot *= mass
 	apply_torque(basis * rot)
 	
-	#var rotaded_thrust: Vector3 = (Vector3.UP * input_thrust * speed_thrust) * global_basis
-	var rotaded_thrust: Vector3 = basis.orthonormalized() * (Vector3.UP * input_thrust * speed_thrust)
-	#apply_central_force(Vector3.UP * input_thrust * speed_thrust)
-	apply_central_force(rotaded_thrust)
-	debug_thrust_pointer.position = basis.orthonormalized() * (4 * Vector3.UP)
+	var thrust: Vector3 = (Vector3.UP * input_thrust * thrust_up)
+	if multiply_by_mass: thrust *= mass
+	apply_central_force(basis * thrust)
+	
+	input_display.text = "rot: " + str(rot) + "\thrust: " + str(thrust)
 	
