a4908deff045c06cb0eb8255f2fedb362127f1e1
Theodor <fracter.giftogod@gmail.com>
2025-07-24 02:31:44 +0200

    attempt at syncing colors on player joined

--- Multiplayer/player_entity_manager.gd
+++ Multiplayer/player_entity_manager.gd
@@ -3,16 +3,11 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	MultiplayerManager.player_joined.connect(sync_player)
 
 
 
-func _player_colors_changed(color:Color, as_primary:bool):
+func _player_colors_changed(_color:Color = Color.WHITE, _as_primary:bool = false):
 	var player: Helicopter = get_heli_of_id(multiplayer.get_unique_id())
 	if player == null: 
 		print("no vehicle found")
@@ -20,13 +15,13 @@ func _player_colors_changed(color:Color, as_primary:bool):
 	
 	player.sync_colors.rpc(%ColorPrimary.color, %ColorSecondary.color)
 
-func _player_custom_name_changed(new_text:String):
+func _player_custom_name_changed(_new_text:String = ""):
 	var player: Helicopter = get_heli_of_id(multiplayer.get_unique_id())
 	if player == null: 
 		print("no vehicle found")
 		return
 	
-	player.sync_name.rpc(new_text)
+	player.sync_name.rpc(%VehicleName.text)
 
 func get_heli_of_id(id:int) -> Helicopter:
 	for child in get_children():
@@ -34,3 +29,8 @@ func get_heli_of_id(id:int) -> Helicopter:
 			if child.player_id == id: return child
 	
 	return null
+
+
+func sync_player():
+	_player_colors_changed()
+	_player_custom_name_changed()
